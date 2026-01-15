41a5a91c338d92a806398752353e61f5aaed8256
Ehwa2006 <165359063+Ehwa2006@users.noreply.github.com>
2026-01-15 14:26:51 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ new Html5Qrcode("reader").start(
   { facingMode: "environment" },
   { fps: 10, qrbox: 220 },
   qrText => {
+
+    const scanned = qrText.trim(); // ⭐ 핵심
+    console.log("스캔된 QR:", scanned);
+
     const spot = Object.values(SPOTS)
-      .find(s => s.qr === qrText);
+      .find(s => s.qr === scanned);
 
     if (!spot) {
       document.getElementById("status").innerText =
-        "등록되지 않은 장소입니다.";
+        `등록되지 않은 장소입니다.\n(${scanned})`;
       return;
     }
 
